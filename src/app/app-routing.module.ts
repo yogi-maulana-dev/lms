780b93e516ad55aa0dde3be815f29efe3cae75e2
yogi-maulana-dev <yogimaulana100@gmail.com>
2023-03-20 01:50:35 +0700

    menggabungkan dua halaman menggunakan route dan path

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule,Routes } from "@angular/router";
+import { HalamanDuaComponent } from "./halaman-dua/halaman-dua.component";
+import { HalamanSatuComponent } from "./halaman-satu/halaman-satu.component";
+
+const routes: Routes=[
+{
+ path:'halaman-satu', component:HalamanSatuComponent
+},
+{
+    path: 'halaman-dua', component:HalamanDuaComponent
+}
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+
+})
+export class AppRoutingModule{}
