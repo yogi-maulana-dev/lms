f59d5e6b5fb8f3b22cbe2879c6be46a546e09cc2
yogi-maulana-dev <yogimaulana100@gmail.com>
2023-03-22 12:34:47 +0700

    menambahkan path halaman utama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HalamanDuaComponent } from "./halaman-dua/halaman-dua.component";
 import { HalamanSatuComponent } from "./halaman-satu/halaman-satu.component";
+import { HalamanUtamaComponent } from "./halaman-utama/halaman-utama.component";
 
 const routes: Routes = [
     {
@@ -9,6 +10,13 @@ const routes: Routes = [
     },
     {
         path: 'halaman-dua', component: HalamanDuaComponent
+    },
+    {
+        path: 'halaman-utama', component: HalamanUtamaComponent
+    },
+    {
+        path: '', redirectTo: 'halaman-utama', pathMatch: 'full'
+
     }
 
 ];
